f9d3a00a6ca40170964a554d62654b4548142a61
vishnugopy <vishnugoby0010@gmail.com>
2023-11-08 19:17:06 +0100

    redesign

--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,12 @@
 import styles from "./main.module.scss";
-import { Badge, NavLink } from "@mantine/core";
+import { Center, SegmentedControl, Box, rem, NavLink } from "@mantine/core";
 import {
   IconHome2,
-  IconGauge,
-  IconChevronRight,
-  IconActivity,
-  IconCircleOff,
+  IconHeart,
+  IconQrcode,
+  IconUser,
+  IconScan,
+  IconSearch,
 } from "@tabler/icons-react";
 
 export default function Main() {
@@ -18,20 +19,43 @@ export default function Main() {
             leftSection={<IconHome2 size="1rem" stroke={1.5} />}
           />
           <NavLink
-            label="With icon"
-            leftSection={<IconHome2 size="1rem" stroke={1.5} />}
+            label="My Codes"
+            leftSection={<IconQrcode size="1rem" stroke={1.5} />}
           />
           <NavLink
-            label="With icon"
-            leftSection={<IconHome2 size="1rem" stroke={1.5} />}
+            label="Favorites"
+            leftSection={<IconHeart size="1rem" stroke={1.5} />}
           />
           <NavLink
-            label="With icon"
-            leftSection={<IconHome2 size="1rem" stroke={1.5} />}
+            label="Profile"
+            leftSection={<IconUser size="1rem" stroke={1.5} />}
           />
         </div>
 
-        <div>content</div>
+        <div className={styles.right}>
+          <SegmentedControl
+            data={[
+              {
+                value: "Scan",
+                label: (
+                  <Center>
+                    <IconScan style={{ width: rem(16), height: rem(16) }} />
+                    <Box ml={10}>Scan</Box>
+                  </Center>
+                ),
+              },
+              {
+                value: "Search",
+                label: (
+                  <Center>
+                    <IconSearch style={{ width: rem(16), height: rem(16) }} />
+                    <Box ml={10}>Search</Box>
+                  </Center>
+                ),
+              },
+            ]}
+          />
+        </div>
       </section>
     </main>
   );
