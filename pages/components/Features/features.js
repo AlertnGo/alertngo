1b0afc62aa31f7f397e6aeb2f83339a07fb29a01
vishnugopy <vishnugoby0010@gmail.com>
2023-11-19 17:48:18 +0100

    Add features component and styles

---
+++ pages/components/Features/features.js
@@ -0,0 +1,106 @@
+import {
+  Badge,
+  Input,
+  Button,
+  Group,
+  Title,
+  Text,
+  Card,
+  SimpleGrid,
+  Container,
+  rem,
+  useMantineTheme,
+  Center,
+} from "@mantine/core";
+import {
+  IconAt,
+  IconTimeDuration30,
+  IconCat,
+  IconAccessPoint,
+} from "@tabler/icons-react";
+import styles from "./featuresCards.module.scss";
+
+const mockdata = [
+  {
+    title: "Plus besoin d'attendre les autres",
+    description:
+      "Si vous êtes pressé et s'ils vous bloquent , vous avez de la chance s'ils utilisent Alerngo, alertez-les immédiatement et allez là où vous devez aller rapidement.",
+    icon: IconTimeDuration30,
+  },
+  {
+    title: "Soyez gentil comme vous l'êtes",
+    description:
+      "Si vous avez trouvé un animal perdu, vous pouvez l'alerter en utilisant Alertngo, il sera très utile pour les propriétaires d'animaux de compagnie.",
+    icon: IconCat,
+  },
+  {
+    title: "Utile pour les personnes l'autre ",
+    description:
+      "Si vous avez besoin de contacter le propriétaire de la voiture, utilisez alertngo. ",
+    icon: IconAccessPoint,
+  },
+];
+
+export default function Features() {
+  const theme = useMantineTheme();
+  const features = mockdata.map((feature) => (
+    <Card
+      key={feature.title}
+      shadow="md"
+      radius="md"
+      className={styles.card}
+      padding="xl"
+    >
+      <feature.icon
+        style={{ width: rem(50), height: rem(50) }}
+        stroke={2}
+        color={theme.colors.blue[6]}
+      />
+      <Text fz="lg" fw={500} className={styles.cardTitle} mt="md">
+        {feature.title}
+      </Text>
+      <Text fz="sm" c="dimmed" mt="sm">
+        {feature.description}
+      </Text>
+    </Card>
+  ));
+
+  return (
+    <Container size="lg" py="xl">
+      <Title order={1} className={styles.title} ta="center" mt="sm" mx="sm">
+        Alertngo
+        <Badge color="blue" variant="light">
+          0.0.1
+        </Badge>
+      </Title>
+
+      <Text
+        c="dimmed"
+        className={styles.description}
+        ta="center"
+        mt="md"
+        mb="xl"
+      >
+        Alertngo est une application web qui permet aux utilisateurs de
+        communiquer avec d'autres personnes à l'aide d'un code Qr.
+      </Text>
+
+      <Text align="center">Rejoignez la liste d'attente</Text>
+      <Center>
+        <Group my={10} align="center">
+          <Input
+            leftSection={<IconAt size={16} />}
+            variant="filled"
+            placeholder="Votre email Ici"
+            size="md"
+          />
+          <Button size="md">M'ajouter</Button>
+        </Group>
+      </Center>
+
+      <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
+        {features}
+      </SimpleGrid>
+    </Container>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,10 @@
 import styles from "./index.module.scss";
-import { Input, Button, Group, Space } from "@mantine/core";
-import { IconAt } from "@tabler/icons-react";
+import Features from "./components/Features/features";
 
 export default function Home() {
   return (
     <main className={styles.main2}>
-      <section className={styles.hero}>
-        <h1>Alertngo</h1>
-        <p>
-          Alertngo est une application web qui permet aux utilisateurs de
-          communiquer avec d'autres personnes à l'aide d'un code Qr.
-        </p>
-        <Space h="md" />
-        <Group>
-          <Input
-            icon={<IconAt />}
-            variant="filled"
-            placeholder="Votre email Ici"
-            size="md"
-          />
-          <Button size="md">S'inscrire sur la liste d'attente</Button>
-        </Group>
-      </section>
+      <Features />
     </main>
   );
 }
