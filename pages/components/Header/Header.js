2409e27d075e519d509bd515f274a52eeaae8430
vishnugopy <vishnugoby0010@gmail.com>
2023-12-19 14:49:05 +0100

    Add mobile menu functionality

--- pages/components/Header/Header.js
+++ pages/components/Header/Header.js
@@ -1,22 +1,19 @@
 // header
 import Link from "next/link";
 import styles from "./header.module.scss";
-import { Button } from "@mantine/core";
-import Toggle from "../Theme/themeToggle";
+import { Button, Drawer, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function Header() {
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <header className={styles.header}>
       <Link href="/">
         <img src="logo.svg" alt="logo" />
       </Link>
-      <nav>
-        <Link href="/main">
-          <Button variant="transparent">App</Button>
-        </Link>
-        <Link href="/price">
-          <Button variant="transparent">Prix</Button>
-        </Link>
+      <nav className={styles.menuPc}>
+        <Link href="/main">App</Link>
+        <Link href="/price">Prix</Link>
         <Link href="/signin">
           <Button variant="outline">Se connecter</Button>
         </Link>
@@ -24,6 +21,29 @@ export default function Header() {
           <Button>S'inscrire </Button>
         </Link>
       </nav>
+
+      <Burger
+        className={styles.menuPhone}
+        opened={opened}
+        onClick={open}
+        aria-label="Toggle navigation"
+      />
+      <Drawer opened={opened} onClose={close} title="Menu">
+        <div className={styles.menulist}>
+          <Link href="/main" onClick={close}>
+            App
+          </Link>
+          <Link href="/price" onClick={close}>
+            Prix
+          </Link>
+          <Link href="/signin" onClick={close}>
+            Se connecter
+          </Link>
+          <Link href="/login" onClick={close}>
+            S'inscrire
+          </Link>
+        </div>
+      </Drawer>
     </header>
   );
 }
