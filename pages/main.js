80269cc1334087c0f3f90f0ffc83784249d1f5a7
vishnugopy <vishnugoby0010@gmail.com>
2023-12-23 00:20:13 +0100

    Remove currentMode state from Main component

--- pages/main.js
+++ pages/main.js
@@ -15,7 +15,6 @@ import MainFavourite from "./mainComponents/mainFavourite";
 
 export default function Main() {
   const router = useRouter();
-  const [currentMode, setCurrentMode] = useState(1);
   return (
     <>
       <div className={styles.mobileNav}>
@@ -23,26 +22,22 @@ export default function Main() {
           onClick={() => router.push("/main?mode=1")}
           size="30px"
           stroke={1.5}
-          color={currentMode === 1 ? "#218BE6" : "#00000082"}
         />
 
         <IconQrcode
           onClick={() => router.push("/main?mode=2")}
           size="30px"
           stroke={1.5}
-          color={currentMode === 2 ? "#218BE6" : "#00000082"}
         />
         <IconHeart
           onClick={() => router.push("/main?mode=3")}
           size="30px"
           stroke={1.5}
-          color={currentMode === 3 ? "#218BE6" : "#00000082"}
         />
         <IconUser
           onClick={() => router.push("/main?mode=4")}
           size="30px"
           stroke={1.5}
-          color={currentMode === 4 ? "#218BE6" : "#00000082"}
         />
       </div>
 
@@ -52,46 +47,22 @@ export default function Main() {
             <NavLink
               onClick={() => router.push("/main?mode=1")}
               label="Trouver"
-              leftSection={
-                <IconUserScan
-                  size="1.2rem"
-                  stroke={1}
-                  color={currentMode === 1 ? "#218BE6" : "#000000"}
-                />
-              }
+              leftSection={<IconUserScan size="1.2rem" stroke={1} />}
             />
             <NavLink
               onClick={() => router.push("/main?mode=2")}
               label="Mes Codes"
-              leftSection={
-                <IconQrcode
-                  size="1.2rem"
-                  stroke={1}
-                  color={currentMode === 2 ? "#218BE6" : "#000000"}
-                />
-              }
+              leftSection={<IconQrcode size="1.2rem" stroke={1} />}
             />
             <NavLink
               onClick={() => router.push("/main?mode=3")}
               label="Favoris"
-              leftSection={
-                <IconHeart
-                  size="1.2rem"
-                  stroke={1}
-                  color={currentMode === 3 ? "#218BE6" : "#000000"}
-                />
-              }
+              leftSection={<IconHeart size="1.2rem" stroke={1} />}
             />
             <NavLink
               onClick={() => router.push("/main?mode=4")}
               label="Profil"
-              leftSection={
-                <IconUser
-                  size="1.2rem"
-                  stroke={1}
-                  color={currentMode === 4 ? "#218BE6" : "#000000"}
-                />
-              }
+              leftSection={<IconUser size="1.2rem" stroke={1} />}
             />
           </div>
 
