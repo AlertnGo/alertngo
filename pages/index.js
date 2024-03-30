4bd8da6e1b2e0d20004a91387ec4d3fb18d6508b
vishnugopy <vishnugoby0010@gmail.com>
2024-03-30 16:05:12 +0100

    testing versel postgr

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Features from "./components/Features/features";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home({ params }) {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
   return (
     <main>
+      <div>
+        {rows.map((row) => (
+          <div key={row.id}>
+            {row.id} - {row.quantity}
+          </div>
+        ))}
+      </div>
       <Features />
     </main>
   );
