e2a13f4f007048ec24cc95caf39033a3f4e1acf7
vishnugopy <vishnugoby0010@gmail.com>
2024-03-30 16:23:31 +0100

    Supprimer la requête SQL et la boucle de rendu des données

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
 import Features from "./components/Features/features";
-import { sql } from "@vercel/postgres";
 
-export default async function Home({ params }) {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+export default async function Home() {
   return (
     <main>
-      <div>
-        {rows.map((row) => (
-          <div key={row.id}>
-            {row.id} - {row.quantity}
-          </div>
-        ))}
-      </div>
       <Features />
     </main>
   );
